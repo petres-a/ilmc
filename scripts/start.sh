1b5c9a99009248184fb455619371f499f2a6ed73
Justkant <quent92100@gmail.com>
2016-07-23 12:17:10 +0200

    fix start.sh api version

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if ! [ -f docker-compose.yml ]; then
-  echo -e "web:\n  image: justkant/ilovemycity-api:0.1.12\n  ports:\n    - \"3030:3030\"\n  links:\n    - \"rdb:webdb\"\n  environment:\n    - DOCKER_HOST=\${DOCKER_HOST}\n\nrdb:\n  image: rethinkdb:2.3\n  volumes_from:\n    - data\n\ndata:\n  image: tianon/true\n  volumes:\n    - /data" >> docker-compose.yml
+  echo -e "web:\n  image: justkant/ilovemycity-api:0.1.9\n  ports:\n    - \"3030:3030\"\n  links:\n    - \"rdb:webdb\"\n  environment:\n    - DOCKER_HOST=\${DOCKER_HOST}\n\nrdb:\n  image: rethinkdb:2.3\n  volumes_from:\n    - data\n\ndata:\n  image: tianon/true\n  volumes:\n    - /data" >> docker-compose.yml
 fi
 docker-compose build && docker-compose up
